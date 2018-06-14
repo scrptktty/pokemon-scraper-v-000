e52f167a30151f6d96de5a211fcd148d29767ab0
Kachel Wittig <kjwittig@gmail.com>
2018-06-14 22:18:29 +0000

    automatically backed up by learn

--- db/schema_migration.sql
+++ db/schema_migration.sql
@@ -1,1 +1,3 @@
 CREATE TABLE IF NOT EXISTS pokemon(id INTEGER PRIMARY KEY, name TEXT, type TEXT);
+-- UPDATE [table name] SET [column name] = [new value] WHERE [column name] = [value];
+UPDATE pokemon SET hp = 60;
