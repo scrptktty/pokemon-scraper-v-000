7a5e363f7f42cab35191253743b6c073bc0df3cc
Kachel Wittig <kjwittig@gmail.com>
2018-06-14 21:29:35 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,19 @@
 class Pokemon
+
+  attr_accessor :name, :type, :db, :id
+
+  def initialize(id: id, name: name, type: type, db: db)
+    @name = name
+    @type = type
+    @db = db
+  end
+
+  def self.save(name, type, db)
+    db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)",name, type)
+  end
+
+  def self.find(id_int, db)
+    db.execute("SELECT * FROM pokemon WHERE pokemon.id = id")
+  end
+
 end
